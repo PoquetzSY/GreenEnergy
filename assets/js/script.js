b359c12ff1e68cad581a4ec0be6c411cb0af3987
PoquetzSY <luisgomez7829@gmail.com>
2023-11-06 17:24:46 -0500

    servicios

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,24 +16,28 @@ window.addEventListener('DOMContentLoaded', () => {
     document.body.insertBefore(headerContainer, document.body.firstChild);
 });
 
+//Firebase
 
-//doughnut
-var ctxD = document.getElementById("doughnutChart").getContext('2d');
-var myLineChart = new Chart(ctxD, {
-  type: 'doughnut',
-  data: {
-    labels: ["Red", "Green", "Yellow", "Grey", "Dark Grey"],
-    datasets: [{
-      data: [300, 50, 100, 40, 120],
-      backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360"],
-      hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870", "#A8B3C5", "#616774"]
-    }]
-  },
-  options: {
-    responsive: true
-  }
-});
 
+// //doughnut
+// var ctxD = document.getElementById("doughnutChart").getContext('2d');
+// var myLineChart = new Chart(ctxD, {
+//   type: 'doughnut',
+//   data: {
+//     labels: ["Red", "Green", "Yellow", "Grey", "Dark Grey"],
+//     datasets: [{
+//       data: [300, 50, 100, 40, 120],
+//       backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360"],
+//       hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870", "#A8B3C5", "#616774"]
+//     }]
+//   },
+//   options: {
+//     responsive: true
+//   }
+// });
+
+
+//calcyladora
 function calcularHuella() {
   // Obtener los valores seleccionados por el usuario
   const consumoEnergia = document.getElementById("consumoEnergia").value;
