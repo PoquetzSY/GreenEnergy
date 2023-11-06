a59c0b290db223af4f505af32a00afcecf26163b
PoquetzSY <luisgomez7829@gmail.com>
2023-11-06 03:29:21 -0500

    Header

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,26 @@
-//pie
-var ctxP = document.getElementById("pieChart").getContext('2d');
-var myPieChart = new Chart(ctxP, {
-  type: 'pie',
+// Crea un elemento div para contener el encabezado
+const headerContainer = document.createElement('header');
+
+// Utiliza el método fetch para cargar el contenido del encabezado desde header.html
+fetch('/view/header.html')
+    .then(response => response.text())
+    .then(data => {
+        headerContainer.innerHTML = data;
+    })
+    .catch(error => {
+        console.error('Error al cargar el encabezado:', error);
+    });
+
+// Agrega el encabezado al cuerpo de la página una vez que esté cargado
+window.addEventListener('DOMContentLoaded', () => {
+    document.body.insertBefore(headerContainer, document.body.firstChild);
+});
+
+
+//doughnut
+var ctxD = document.getElementById("doughnutChart").getContext('2d');
+var myLineChart = new Chart(ctxD, {
+  type: 'doughnut',
   data: {
     labels: ["Red", "Green", "Yellow", "Grey", "Dark Grey"],
     datasets: [{
@@ -14,3 +33,43 @@ var myPieChart = new Chart(ctxP, {
     responsive: true
   }
 });
+
+function calcularHuella() {
+  // Obtener los valores seleccionados por el usuario
+  const consumoEnergia = document.getElementById("consumoEnergia").value;
+  const transporte = document.getElementById("transporte").value;
+  const dieta = document.getElementById("dieta").value;
+
+  // Realizar cálculos de huella de carbono (ficticios) en base a las opciones seleccionadas
+  let huellaCarbono = 0;
+
+  if (consumoEnergia === "bajo") {
+      huellaCarbono += 0.1;
+  } else if (consumoEnergia === "medio") {
+      huellaCarbono += 0.2;
+  } else if (consumoEnergia === "alto") {
+      huellaCarbono += 0.3;
+  }
+
+  if (transporte === "corto") {
+      huellaCarbono += 0.05;
+  } else if (transporte === "medio") {
+      huellaCarbono += 0.1;
+  } else if (transporte === "largo") {
+      huellaCarbono += 0.2;
+  }
+
+  if (dieta === "pocaCarne") {
+      huellaCarbono += 0.1;
+  } else if (dieta === "moderadaCarne") {
+      huellaCarbono += 0.2;
+  } else if (dieta === "muchaCarne") {
+      huellaCarbono += 0.3;
+  }
+
+  // Calcular la huella de carbono mensual (multiplicar por 30 para un mes)
+  const huellaMensual = (huellaCarbono).toFixed(2);
+
+  // Mostrar el resultado
+  document.getElementById("resultado").textContent = huellaMensual + " toneladas de CO2";
+}
